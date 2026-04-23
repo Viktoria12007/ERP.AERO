794ac14d76c3e3d743b04dc1756502fa3b49d83c
Golubova Viktoriia <viki12007@inbox.ru>
2026-04-23 13:38:45 +0600

    Added app.use: cors, express.json(), routes.

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,13 @@
 import express from 'express';
+import cors from "cors";
+import routes from "./routes";
 
 const { PORT = 3000 } = process.env;
 const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
 
 app.listen(PORT, () => {
     console.log(`Сервер запущен на http://localhost:${PORT}`);
