112399dd787a81fac7a0471f5a55eb1f3e657171
Golubova Viktoriia <viki12007@inbox.ru>
2026-04-23 13:26:41 +0600

    Created PrismaClient.

---
+++ prisma/client.ts
@@ -0,0 +1,10 @@
+import { PrismaClient } from "../src/generated/prisma/client";
+import { PrismaMariaDb } from "@prisma/adapter-mariadb";
+
+const adapter = new PrismaMariaDb({
+  host: "localhost",
+  user: "root",
+  database: "mydb",
+});
+
+export const prisma = new PrismaClient({ adapter });
