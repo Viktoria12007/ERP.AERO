fac886eb63beef98050e39d9622cace2f4783cb2
Golubova Viktoriia <viki12007@inbox.ru>
2026-04-23 13:32:59 +0600

    Created users controller.

---
+++ src/controllers/users.ts
@@ -0,0 +1,18 @@
+import { Request, Response } from 'express';
+import * as userService from '../services/users';
+
+export const getInfoUserHandler = async (req: Request, res: Response) => {
+  try {
+    const user = await userService.getUser(req.body);
+    res.json(user.id);
+  } catch (err) {
+    if (err.code === 'P2025') {
+      res.status(404).json({ error: 'User not found' });
+    }
+  }
+};
+
+export const logoutHandler = async (req: Request, res: Response) => {
+  await userService.logout(req.body);
+  res.json('ok');
+}
