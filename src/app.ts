d6a25deda82bd315f90b97ba52f276749c3f45aa
Golubova Viktoriia <viki12007@inbox.ru>
2026-04-24 19:34:28 +0600

    Added using cookieParser() and errorHandler.

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import cors from "cors";
 import routes from "./routes/index.js";
+import errorHandler from "./middlewares/error-handler.js";
+import cookieParser from "cookie-parser";
 
 const { PORT = 3000 } = process.env;
 const app = express();
@@ -8,7 +10,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 app.use(routes);
+app.use(errorHandler);
 
 app.listen(PORT, () => {
     console.log(`Сервер запущен на http://localhost:${PORT}`);
