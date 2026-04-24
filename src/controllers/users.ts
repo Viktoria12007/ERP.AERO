58ab0d93df03ab627794783dfd97b94dd967e3c9
Golubova Viktoriia <viki12007@inbox.ru>
2026-04-24 15:02:56 +0600

    Added hashing password while create user.

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,8 +1,21 @@
-import { Request, Response } from 'express';
-import * as userService from '../services/users';
 import type { Request, Response } from 'express';
 import * as userService from '../services/users.js';
 import bcrypt from 'bcryptjs';
+
+export const createUserHandler = async (req: Request, res: Response) => {
+  try {
+    const { id, password } = req.body;
+    const hash = await bcrypt.hash(password, 10);
+    const newUser = await userService.createUser({ id, password: hash });
+    res.status(201).json(newUser);
+  } catch {
+    res.status(500).json({ error: 'fail' });
+  }
+}
+
+export const loginHandler = async (req: Request, res: Response) => {
+
+}
 
 export const getInfoUserHandler = async (req: Request, res: Response) => {
   try {
