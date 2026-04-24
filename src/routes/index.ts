846243304f8c78a4c33606e5690b884f1639309a
Golubova Viktoriia <viki12007@inbox.ru>
2026-04-24 19:35:37 +0600

    Added using middleware 'auth' and 'NotFoundError'.

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,15 +1,22 @@
-import { Router } from 'express';
+import { Router, type Request, type Response, type NextFunction } from 'express';
 import userRouter from './users.js';
 import fileRouter from './files.js';
-import { createUserHandler, loginHandler } from "../controllers/users.js";
+import { createUserHandler, getNewTokenHandler, loginHandler } from "../controllers/users.js";
+import NotFoundError from "../errors/not-found-error.js";
+import auth from "../middlewares/auth.js";
 
 const router = Router();
 
 router.post('/signup', createUserHandler);
 router.post('/signin', loginHandler);
-router.post('/signin/new_token', loginHandler);
+router.post('/signin/new_token', getNewTokenHandler);
 
+router.use(auth);
 router.use('/', userRouter);
 router.use('/file', fileRouter);
+
+router.use((req: Request, res: Response, next: NextFunction) => {
+  next(new NotFoundError('Маршрут не найден'));
+});
 
 export default router;
