8f14367f38070975fd635eef115061c7fd7b4a74
Golubova Viktoriia <viki12007@inbox.ru>
2026-04-24 14:23:02 +0600

    Added using 'express.urlencoded'.

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,13 @@
 import express from 'express';
 import cors from "cors";
-import routes from "./routes";
+import routes from "./routes/index.js";
 
 const { PORT = 3000 } = process.env;
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 app.listen(PORT, () => {
