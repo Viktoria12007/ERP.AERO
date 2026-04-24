aaae833d9e419338cac1d9c80d25622e63dee1a5
Golubova Viktoriia <viki12007@inbox.ru>
2026-04-24 14:55:10 +0600

    Added service 'createUser'.

--- src/services/users.ts
+++ src/services/users.ts
@@ -1,7 +1,11 @@
-import { prisma } from "../../prisma/client";
+import { prisma } from "../../lib/prisma.js";
 
-export const getUser = async (id: string) => {
-  return prisma.user.findUniqueOrThrow({ where: { id } });
+export const createUser = async (user: { id: string, password: string }) => {
+  return prisma.user.create({ data: user });
+}
+
+export const getUser = async () => {
+  return prisma.user.findFirstOrThrow();
 }
 
 export const logout = async (id: string) => {
