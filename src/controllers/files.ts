5da315bfe0d2e98cfa6169e25520a23848752acd
Golubova Viktoriia <viki12007@inbox.ru>
2026-04-28 11:22:18 +0600

    Created controller 'uploadFileHandler'.

--- src/controllers/files.ts
+++ src/controllers/files.ts
@@ -1,5 +1,6 @@
-import type { Request, Response } from "express";
+import type { NextFunction, Request, Response } from "express";
 import * as fileService from "../services/files.js";
+import path from "path";
 
 export const getFilesHandler = async (req: Request, res: Response) => {
     const { listSize, page } = req.query;
@@ -18,21 +19,27 @@ export const getFileHandler = async (req: Request, res: Response) => {
   }
 };
 
-export const downloadFileHandler = async (req: Request, res: Response) => {
-  // try {
-  //   const user = await fileService.getUser(req.body);
-  //   res.json(user.id);
-  // } catch (err) {
-  //   if (err.code === 'P2025') {
-  //     res.status(404).json({ error: 'User not found' });
-  //   }
-  // }
+export const downloadFileHandler = async (req: Request, res: Response, next: NextFunction) => {
+
 };
 
-export const createFileHandler = async (req: Request, res: Response) => {
+export const uploadFileHandler = async (req: Request, res: Response) => {
   try {
-    const file = await fileService.createFile(req.body);
-    res.status(201).json(file);
+    const file = req.file;
+
+    if (!file) {
+      return res.status(400).json({ message: 'No file uploaded' });
+    }
+
+    const ext = path.extname(file.originalname).replace('.', '');
+    const newFile = await fileService.createFile({
+      name: file.filename,
+      extension: ext,
+      mimeType: file.mimetype,
+      size: file.size,
+    });
+
+    res.status(201).json(newFile);
   } catch {
     res.status(400).json({ error: 'File creation failed' });
   }
