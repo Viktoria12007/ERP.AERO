ba90a1f33723d54400c65dc341830909b16daccc
Golubova Viktoriia <viki12007@inbox.ru>
2026-04-28 11:20:20 +0600

    Added using middleware "upload".

--- src/routes/files.ts
+++ src/routes/files.ts
@@ -1,19 +1,20 @@
 import { Router } from 'express';
 import {
-  createFileHandler,
+  uploadFileHandler,
   deleteFileHandler,
   downloadFileHandler,
   getFileHandler,
   getFilesHandler,
   updateFileHandler
 } from "../controllers/files.js";
+import { upload } from "../middlewares/upload";
 
 const router = Router();
 
 router.get('/list', getFilesHandler);
 router.get('/:id', getFileHandler);
 router.get('/download/:id', downloadFileHandler);
-router.post('/upload', createFileHandler);
+router.post('/upload', upload.single('file'), uploadFileHandler);
 router.put('/update/:id', updateFileHandler);
 router.delete('/delete/:id', deleteFileHandler);
 
